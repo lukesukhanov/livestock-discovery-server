6847ff9df5ef989b6192baa3f4b2ade97fb85480
Luke Sukhanov <lukesukhanov@gmail.com>
2023-07-05 14:11:59 +0300

    Add basic config

---
+++ src/main/java/com/livestockshop/discoveryserver/LivestockShopDiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.livestockshop.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LivestockShopDiscoveryServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LivestockShopDiscoveryServerApplication.class, args);
+  }
+}
