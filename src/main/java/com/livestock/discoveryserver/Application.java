0b16e9ff6680d0b4a971f61a43a017d35ab68ac5
Luke Sukhanov <lukesukhanov@gmail.com>
2023-07-14 03:58:43 +0300

    Cosmetic changes

--- src/main/java/com/livestock/discoveryserver/Application.java
+++ src/main/java/com/livestock/discoveryserver/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class LivestockShopDiscoveryServerApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(LivestockShopDiscoveryServerApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
